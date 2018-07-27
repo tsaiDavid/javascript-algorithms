5b31ba6b7304aef599d0dd05716b676cdbf28140
David Tsai <david@whataniceguy.com>
2018-07-26 17:07:56 -0700

    Working through toString

--- src/data-structures/linked-list/LinkedList.js
+++ src/data-structures/linked-list/LinkedList.js
@@ -1,11 +1,32 @@
 import LinkedListNode from './LinkedListNode';
 
 export default class LinkedList {
-  constructor(value = '') {
-    this.value = new LinkedListNode(value);
+  constructor(value) {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+    if (!this.tail && !this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = this.tail.next;
+    }
   }
 
   toString() {
-    return this.value;
+    let node = this.head;
+    let retStr = '';
+
+    while (node && node.next) {
+      console.log(node)
+      retStr += node.toString();
+      node = node.next;
+    }
+
+    return retStr;
   }
 }
